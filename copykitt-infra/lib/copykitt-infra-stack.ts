f29dca7009951b3f47c3ca5f617bedfe27d84ec3
leifWoe <woehlk.lennart@gmail.com>
2023-05-04 16:44:24 +0200

    lambda api gateway

--- copykitt-infra/lib/copykitt-infra-stack.ts
+++ copykitt-infra/lib/copykitt-infra-stack.ts
@@ -1,7 +1,9 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 import * as dotenv from 'dotenv';
+import { ApiGateway } from 'aws-cdk-lib/aws-events-targets';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 dotenv.config();
@@ -24,5 +26,15 @@ export class CopykittInfraStack extends cdk.Stack {
         "OPENAI_API_KEY": process.env.OPENAI_API_KEY ?? "",
       },
     });
+
+    const copykittAPI = new apigateway.RestApi(this, "RestAPI", {
+      restApiName: "CopyKitt Tutorial API",
+    });
+ 
+    copykittAPI.root.addProxy({
+      defaultIntegration: new apigateway.LambdaIntegration(apiLambda),
+    });
   }
 }
+
+// https://knde0xjtj4.execute-api.eu-central-1.amazonaws.com/prod/generate_copykitt_output?prompt=%22boxing%20club%22
