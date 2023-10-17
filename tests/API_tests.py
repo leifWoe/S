1c6183159924e6d4208edfbb97bc499a5b667f8a
leifWoe <woehlk.lennart@gmail.com>
2023-10-17 19:55:54 +0200

    tests

---
+++ tests/API_tests.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+
+
+class TryTesting(TestCase):
+    def test_always_passes(self):
+        self.assertTrue(True)
+
+    def test_always_fails(self):
+        self.assertTrue(False)
+        
+    def test_snippet_generated(self):
+        self.assertTrue(True)
